01f63f5777780693dcbd783e1dbb2be48768dfc8
Andre Serudo <andreserudo@gmail.com>
2021-03-25 23:37:36 -0400

    Adicao de testes de tipo de entrada

--- src/components/SelectPeriod/SelectPeriod.test.js
+++ src/components/SelectPeriod/SelectPeriod.test.js
@@ -7,17 +7,19 @@ import { theme } from '../../themes';
 import SelectPeriod from './SelectPeriod';
 
 describe('o <SelectPeriod /> deve', () => {
-  it('ter um label, dois inputs e um select', () => {
+  it('ter dois labels, dois inputs e um select', () => {
     const { getByLabelText, getAllByRole, getByRole } = render(
       <ThemeProvider theme={theme}>
         <SelectPeriod />
       </ThemeProvider>,
     );
     const label = getByLabelText('Anos de Experiência:');
+    const labelE = getByLabelText('e');
     const inputs = getAllByRole('textbox');
     const select = getByRole('combobox');
 
     expect(label).toBeInTheDocument();
+    expect(labelE).toBeInTheDocument();
     expect(inputs.length).toBe(2);
     expect(select).toBeInTheDocument();
   });
@@ -52,5 +54,42 @@ describe('o <SelectPeriod /> deve', () => {
     expect(inputs.length).toBe(1);
     userEvent.selectOptions(select, [options[0].textContent]);
     expect(inputs.length).toBe(2);
+  });
+
+  it('deve aceitar somente números para filtro por intervalo', () => {
+    const { getAllByRole } = render(
+      <ThemeProvider theme={theme}>
+        <SelectPeriod />
+      </ThemeProvider>,
+    );
+
+    const inputs = getAllByRole('textbox');
+
+    userEvent.type(inputs[0], 'a');
+    userEvent.type(inputs[0], '1');
+    userEvent.type(inputs[1], 'b');
+    userEvent.type(inputs[1], '2');
+
+    expect(inputs[0]).toHaveValue('1');
+    expect(inputs[1]).toHaveValue('2');
+  });
+
+  it('deve aceitar somente números para filtro por limite', () => {
+    const { getAllByRole, getByRole } = render(
+      <ThemeProvider theme={theme}>
+        <SelectPeriod />
+      </ThemeProvider>,
+    );
+
+    const select = getByRole('combobox');
+    const options = getAllByRole('option');
+    const inputs = getAllByRole('textbox');
+
+    userEvent.selectOptions(select, [options[0].textContent]);
+
+    userEvent.type(inputs[0], 'a');
+    userEvent.type(inputs[0], '1');
+
+    expect(inputs[0]).toHaveValue('1');
   });
 });
